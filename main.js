c04ff40283121341ed52d9250ee3875c84013a81
LucasCoder <lucasgrispoo@gmail.com>
2023-12-13 23:57:45 -0300

    objeto, arrays

--- main.js
+++ main.js
@@ -1,57 +1,77 @@
 
-//Algoritmo con un condicional 
-
-
 let hora = prompt("Ingrese la hora")
 
 if (hora >= 6 && hora < 12) {
 
+
     alert ("Buenos dias")
+
 
 }  else if (hora >= 12 && hora < 20) {
 
+
     alert ("Buenas tardes")
+
 
 }  else if (hora >=20 && hora < 00 ){
 
+
     alert ("Buenas noches") 
 
+
 }  else {
-    
+
+
     alert ("ANDÁ A DORMIR")
 
 }
 
 
-
-
-
-
-
-
-//Algortimo con un ciclo
-
-let x = 0;
-
-while (x<10) {
-
-    console.log("Ciclo número" + x);
-    alert("ahora el x vale" + x);
-    x = x + 1;
     
+    
+    
+    
+    
+const symbols = ['🍒', '🍊', '🍋', '🍇', '🍉'];
+
+const combinations = [
+    { symbols: ['🍒', '🍒', '🍒'], winMessage: 'Ganaste el gran premio.' },
+    { symbols: ['🍊', '🍊', '🍊'], winMessage: '¡Ganaste! Tres naranjas.' },
+    { symbols: ['🍋', '🍋', '🍋'], winMessage: '¡Ganaste! Tres limones.' },
+    { symbols: ['🍇', '🍇', '🍇'], winMessage: '¡Ganaste! Tres uvas.' },
+    { symbols: ['🍉', '🍉', '🍉'], winMessage: '¡Ganaste! Tres sandías.' }
+];
+
+class SlotMachine {
+    constructor() {
+        this.reels = document.querySelectorAll('.reel');
+        this.spinBtn = document.getElementById('spin-btn');
+        this.spinBtn.addEventListener('click', () => this.spin());
+    }
+
+    spin() {
+        this.reels.forEach((reel) => {
+            const randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+            reel.textContent = randomSymbol;
+        });
+
+        this.checkWin();
+    }
+
+    checkWin() {
+        const symbolsArray = Array.from(this.reels).map((reel) => reel.textContent);
+
+        const winningCombination = combinations.find((combination) => {
+            return combination.symbols.every((symbol, index) => symbol === symbolsArray[index]);
+        });
+
+        if (winningCombination) {
+            alert(winningCombination.winMessage);
+        } else {
+            alert('Intenta de nuevo');
+        }
+    }
 }
 
-                      
-            
-    
-const symbols = ['🍒', '🍋', '🍇', '🍊', '🍉'];
-
-function spin() {
-    const reels = document.querySelectorAll('.reel');
-
-    reels.forEach((reel) => {
-        const randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
-        reel.textContent = randomSymbol;
-    });
-}
+const slotMachine = new SlotMachine();
 
